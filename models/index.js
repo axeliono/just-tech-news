e262fed5afab7ac85c8b44b0d337229dd7dcac98
axeliono <69375446+axeliono@users.noreply.github.com>
2021-01-01 12:29:38 -0600

    added voting

--- models/index.js
+++ models/index.js
@@ -1,12 +1,37 @@
 const User = require("./User");
 const Post = require("./Post");
+const Vote = require("./Vote");
 
 // associations
 User.hasMany(Post, {
   foreignKey: "user_id",
 });
+User.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+Post.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+
 Post.belongsTo(User, {
   foreignKey: "user_id",
 });
+Vote.belongsTo(User, {
+  foreignKey: "user_id",
+});
+Vote.belongsTo(Post, {
+  foreignKey: "post_id",
+});
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "user_id",
+});
+Post.belongsToMany(Post, {
+  through: Vote,
+  as: "voted_posts",
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Vote };
